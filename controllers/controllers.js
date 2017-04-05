854347778615fc9b4d24d44e779749efb6b4e895
Lee Morris <leebenmorris@gmail.com>
2017-04-05 16:05:59 +0100

    comment count added to restaurant listing - Extra task 1

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,19 +2,10 @@ const bluebird = require('bluebird');
 const pgp = require('pg-promise')({ promiseLib: bluebird });
 const dbCredentials = require('../db_config.js');
 const db = pgp(dbCredentials);
-
-const allAreas = 'SELECT * FROM areas';
-
-const findRestaurantsByAreaId = id =>
-  `SELECT * FROM restaurants
-   WHERE area_id = ${id}`;
-
-
-
-
+const async = require('async');
 
 function selectAllAreas(req, res) {
-  db.query(allAreas)
+  db.query('SELECT * FROM areas')
     .then(rows => {
       res.status(200).json({ areas: rows });
     })
@@ -24,10 +15,23 @@ function selectAllAreas(req, res) {
 }
 
 function selectRestaurantsByAreaId(req, res) {
-  db.query(findRestaurantsByAreaId(req.params.area_id))
-    .then(rows => {
-      rows = rows.reduce((acc, row) => { acc[row.id] = row; return acc; }, {});
-      res.status(200).json({ restaurants: rows });
+  db.query(`SELECT * FROM restaurants WHERE area_id = $1`, req.params.area_id)
+    .then(restaurants => {
+      async.map(
+        restaurants,
+        function (restaurant, cb) {
+          db.any('SELECT COUNT(*) FROM comments WHERE restaurant_id = $1', restaurant.id)
+            .then(commentsCount => {
+              restaurant.comment_count = +commentsCount[0].count;
+              cb(null, restaurant);
+            });
+        },
+        function (err, docs) {
+          if (err) console.log(err);
+          docs = docs.reduce((acc, restaurant) => { acc[restaurant.id] = restaurant; return acc; }, {});
+          res.status(200).send(docs);
+        }
+      );
     })
     .catch(err => {
       console.log(err);
@@ -61,8 +65,8 @@ function selectCommentsByRestaurantId(req, res) {
     });
 }
 
-function selectRatingsByRestaurantId (req, res) {
-   db.one('SELECT * FROM restaurants WHERE id = $1', req.params.restaurant_id)
+function selectRatingsByRestaurantId(req, res) {
+  db.one('SELECT * FROM restaurants WHERE id = $1', req.params.restaurant_id)
     .then(restaurant => {
       db.any('SELECT * FROM ratings WHERE restaurant_id = $1', restaurant.id)
         .then(ratingRows => {
@@ -76,22 +80,21 @@ function selectRatingsByRestaurantId (req, res) {
     });
 }
 
-
-function postCommentByRestaurantId (req, res) {
+function postCommentByRestaurantId(req, res) {
   db.one('INSERT INTO comments (restaurant_id, body) VALUES ($1, $2) returning *', [req.params.restaurant_id, req.body.comment])
-  .then(comment => {
-    res.status(200).json({posted: comment});
-  })
+    .then(comment => {
+      res.status(200).json({ posted: comment });
+    })
     .catch(err => {
       console.log(err);
     });
 }
 
-function postRatingByRestaurantId (req, res) {
+function postRatingByRestaurantId(req, res) {
   db.one('INSERT INTO ratings (restaurant_id, rating) VALUES ($1, $2) returning *', [req.params.restaurant_id, req.body.rating])
-  .then(rating => {
-    res.status(200).json({posted: rating});
-  })
+    .then(rating => {
+      res.status(200).json({ posted: rating });
+    })
     .catch(err => {
       console.log(err);
     });
